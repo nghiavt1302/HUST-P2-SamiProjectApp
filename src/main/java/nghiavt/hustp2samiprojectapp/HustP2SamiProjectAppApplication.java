9449091988f9e364665a39ad8b494b40539ceeb2
nghiavt1302 <vtnghia01.bchkl@gmail.com>
2023-01-19 16:58:31 +0700

    Create prj

---
+++ src/main/java/nghiavt/hustp2samiprojectapp/HustP2SamiProjectAppApplication.java
@@ -0,0 +1,13 @@
+package nghiavt.hustp2samiprojectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustP2SamiProjectAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HustP2SamiProjectAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nghiavt/hustp2samiprojectapp/HustP2SamiProjectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nghiavt.hustp2samiprojectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustP2SamiProjectAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
